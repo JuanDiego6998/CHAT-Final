dc33887b6a12857cb78c522002fe2746608ae08b
JuanDiego6998 <juandiegogarzon98@hotmail.com>
2018-05-15 12:10:49 -0500

    fors Signed-off-by: JuanDiego6998 <juandiegogarzon98@hotmail.com>

---
+++ ProyectoFinal/login_baseDatos.php
@@ -0,0 +1,27 @@
+<?php
+
+try{
+            $pdo=new PDO('mysql:host=localhost; dbname=chat; charset=utf8', 'chatAdmin', 'chat1234'); // conecccion a la base de datos, llamada chat bajo las credenciales especificadas
+            //echo "Conectado";
+        } catch (Exception $ex) {
+            echo "No conectado".$ex;
+        }
+ 
+if(isset($_POST['userName'])){
+    
+    $uname=$_POST['userName'];
+    $password=$_POST['password'];
+    
+    $sql="select * from usuarios where userName='".$uname."'AND userPass='".$password."' limit 1";
+    
+    $result= $pdo->query($sql);
+    
+    if($row=$result->fetch()){
+        header('Location: account.html.php'); 
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+    }
+        
+}
+?>
